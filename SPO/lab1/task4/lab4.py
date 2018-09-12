a864b1d93cb2992dc40d4f7cabcfc09e9bf46cf5
OlegSouzdalev <oleg.souzdalev@yahoo.fr>
2018-09-12 08:55:37 +0300

    done

--- SPO/lab1/task4/lab4.py
+++ SPO/lab1/task4/lab4.py
@@ -1,9 +1,12 @@
 N = int(input("Enter N\n"))
 Y = N / 24
-A_1 = float(input("Enter probability for Dasha\n"))
-A_2 = float(input("Enter probability for Igor\n"))
-A_3 = float(input("Enter probability for Kirill\n"))
+S = A_1 = A_2 = A_3 = 0
 
-print("invalid probability sum") if A_1 + A_2 + A_3 != 1.0 else 0
+while S != 1:
+    A_1 = float(input("Enter probability for Dasha\n"))
+    A_2 = float(input("Enter probability for Igor\n"))
+    A_3 = float(input("Enter probability for Kirill\n"))
+    S = A_1 + A_2 + A_3
+    print("invalid probability sum") if S != 1.0 else 0
 
-print((A_1 * Y) ** 300)
+print("Answer ", (A_1 * Y) ** 300)
