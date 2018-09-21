8cf335d81039f824a57dd06b012db6c10f25276c
AlexKaravaev <AlexKaravev@gmail.com>
2018-09-21 22:19:26 +0300

    Poly regr added and few comments

--- SPO/lab2/task1/MNK.py
+++ SPO/lab2/task1/MNK.py
@@ -22,12 +22,15 @@ def linRegr(x,y):
     b = (np.sum(y) - a * np.sum(x)) / n
     return a,b
 
+def polynomRegr(x,y,degree=2):
+    return np.poly1d(np.polyfit(x,y,deg=degree))
 
 if __name__ == '__main__':
     ### Read spreadsheet id from file
     f = open("spreadsheetId.txt", "r")
     ssId = f.read()
 
+    ### Get x and y from table and preproccess data
     result = service.spreadsheets().values().get(spreadsheetId=ssId,
                                                 range="1!A1:B"+str(variant_num)).execute()
     values = result.get('values', [])
@@ -35,9 +38,21 @@ if __name__ == '__main__':
     values = np.array(values)
     x,y = values[:,0],values[:,1]
 
+    ### Do linear regression and calculate error
     a,b = linRegr(x,y)
     print("Коэфициенты аппр.прямой y = k*x + b: k={}, b = {}".format(a,b))
     plt.plot(x,y,'g^')
-    X_range = range(-100,100)
-    plt.plot(range(-100,100), a*X_range + b)
+    X_range = np.arange(x.min(),x.max())
+    error_lin = sum([abs((a*x[i] + b) - y[i]) for i in range(len(x))])
+    print("Ошибка в линейной аппроксимации: {}".format(error_lin))
+    plt.plot(X_range, a*X_range + b, label = 'Линейная')
+    ### Now do poly.regr until 5 degree
+    for i in range(2,7):
+        poly_func = polynomRegr(x,y,i)
+        error_poly = sum([abs(poly_func(x[i]) - y[i]) for i in range(len(x))])
+        print("Ошибка в полиномиальной аппроксимации степени {}: {}".format(i,error_poly))
+        plt.plot(X_range, poly_func(X_range),label = '{} степень'.format(i))
+
+    ### Plot
+    plt.legend(loc = 'upper left')
     plt.show()
