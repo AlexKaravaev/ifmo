5f03c7109551c80ff59319a08936a7d12be5bcbf
AlexKaravaev <karavaev-sasha@mail.ru>
2019-09-04 23:35:13 +0300

    Finished contra-harmonic and gaussian filtering

--- CV/lab1/lab.py
+++ CV/lab1/lab.py
@@ -1,9 +1,28 @@
 import cv2
 import numpy as np
 import matplotlib
+from PIL import Image
 from matplotlib import pyplot as plt
 from skimage.util import random_noise
+from skimage import img_as_ubyte
 matplotlib.rcParams["backend"] = "TkAgg"
+np.set_printoptions(threshold=np.inf)
+
+def filter(img: np.ndarray, filter: np.ndarray) -> np.ndarray:
+    """ Generic function, that applies filter-mask to each pixel of image
+    
+    Arguments:
+        img {np.ndarray} -- input image
+        filter {np.ndarray} -- filter array
+    
+    Returns:
+        np.ndarray -- filtered image
+    """
+    h = w = img.shape[0]
+    f_h, f_w = filter.shape[0], filter.shape[1]
+
+    flat_filter = filter.ravel()
+    return np.correlate(img, flat_filter, 'same')
 
 def contraharmonic_filter(img: np.ndarray, Q: int, mask_size: tuple) -> np.ndarray:
     """ Apply contraharmonic filter to input image
@@ -16,23 +35,31 @@ def contraharmonic_filter(img: np.ndarray, Q: int, mask_size: tuple) -> np.ndarr
     Returns:
         np.ndarray -- filtered image
     """
-    h, w = img.shape[0], img.shape[1]
-    filtered = np.zeros(shape = img.shape)
-    for row in range(mask_size[0]//2 , h - mask_size[0]//2 - 1):
-        for col in range(mask_size[1] // 2, w - mask_size[1]//2 - 1):
-            kernel = np.array([img[row + i][col + j] for j in range(-mask_size[0]//2, (mask_size[0] // 2) + 1)
-                            for i in range(-mask_size[1] // 2, (mask_size[1] // 2) + 1)])
-            num   = np.sum(kernel ** (Q + 1))
-            denum = np.sum(kernel ** Q)
-            filtered[row][col] = num / denum
-    return filtered
     
-if __name__=="__main__":
-    img = cv2.imread('../img.jpeg', 0); 
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    gauss = random_noise(img, mode='gaussian', seed=None, clip=True)
+    data = np.array(img, dtype=np.float64)
+    data = data.ravel()
+    num = np.power(data, Q + 1)
+    den = np.power(data, Q)
+    kernel = np.full(mask_size, 1.0)
 
-    cntrharmonic = contraharmonic_filter(img, 1,(3,3))
-    cv2.imshow('original', img)
-    cv2.imshow('contraharmonic_filter', cntrharmonic)
-    cv2.waitKey()
+    res = filter(num, kernel)/filter(den,kernel)
+    return res.reshape(img.shape).astype(np.uint8)
+
+if __name__=="__main__":
+    img = cv2.imread('../img.jpeg', 0)
+    
+    # Apply noise to input image
+    gauss = random_noise(img, mode='gaussian', seed=None, clip=True)
+    gauss = img_as_ubyte(gauss)
+    dir_  = './res/'
+    cv2.imwrite(dir_ + 'gauss.jpeg', gauss)
+
+    Q = [-1, 0, 1]
+    for q in Q:
+        cntrharmonic = contraharmonic_filter(gauss, q,(3,3))
+        cv2.imwrite(dir_ + 'contraharmonic_Q_' + str(q) + '.jpg', cntrharmonic)
+    
+    for q in Q:
+        gauss_blur = cv2.GaussianBlur(gauss, (3,3), q)
+        cv2.imwrite(dir_ + 'gaussian_Q_' + str(q) + '.jpg', gauss_blur)
+
