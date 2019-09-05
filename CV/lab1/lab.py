f3f508f0b396203e5ca9bb66e30a29b7a2ee1dd0
AlexKaravaev <alexkaravev@gmail.com>
2019-09-05 11:33:40 +0300

    rank, wiener, mean finished

--- CV/lab1/lab.py
+++ CV/lab1/lab.py
@@ -5,6 +5,9 @@ from PIL import Image
 from matplotlib import pyplot as plt
 from skimage.util import random_noise
 from skimage import img_as_ubyte
+from scipy.signal import wiener
+from scipy.ndimage import rank_filter as _rank_filter
+
 matplotlib.rcParams["backend"] = "TkAgg"
 np.set_printoptions(threshold=np.inf)
 
@@ -45,6 +48,36 @@ def contraharmonic_filter(img: np.ndarray, Q: int, mask_size: tuple) -> np.ndarr
     res = filter(num, kernel)/filter(den,kernel)
     return res.reshape(img.shape).astype(np.uint8)
 
+def wiener_filter(img: np.ndarray, mask_size: tuple) -> np.ndarray:
+    """ Apply wiener filtering to image
+    
+    Arguments:
+        img {np.ndarray} -- input image
+        mask_size {tuple} -- convolution mask size
+    
+    Returns:
+        np.ndarray -- fiktered image
+    """
+    data = np.array(img, dtype=np.float64)
+    data = data.ravel()
+    res  = wiener(data, mask_size[0])
+    return res.reshape(img.shape).astype(np.uint8)
+
+def rank_filter(img: np.ndarray, rank: int, mask_size: tuple) -> np.ndarray:
+    """ Apply rank filtering to image
+    
+    Arguments:
+        img {np.ndarray} -- input image
+        rank {int} -- rank parameter
+        mask_size{tuple} -- footprint size
+    Returns:
+        np.ndarray -- filtered image
+    """
+    data = np.array(img, dtype=np.float64)
+    data = data.ravel()
+    res  = _rank_filter(input=data, rank=rank, footprint=mask_size)
+    return res.reshape(img.shape).astype(np.uint8)
+     
 if __name__=="__main__":
     img = cv2.imread('../img.jpeg', 0)
     
@@ -63,3 +96,13 @@ if __name__=="__main__":
         gauss_blur = cv2.GaussianBlur(gauss, (3,3), q)
         cv2.imwrite(dir_ + 'gaussian_Q_' + str(q) + '.jpg', gauss_blur)
 
+    # Median, weighted median, rang and Winner filtering
+    cv2.imwrite(dir_ + 'median.jpg', cv2.medianBlur(gauss, 1))
+    
+    cv2.imwrite(dir_ + 'weighted_median.jpg', cv2.medianBlur(gauss, 3))
+    
+    cv2.imwrite(dir_ + 'wiener.jpg', wiener_filter(gauss, (3,3)))
+   
+    cv2.imwrite(dir_ + 'rank.jpg', rank_filter(gauss, -1, (3,3)))
+    
+    
